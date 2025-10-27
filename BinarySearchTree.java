6d6fa01ad9ca4d6b3573f9096119bdc5068fd72f
abickham1 <abickham2020@gmail.com>
2025-10-27 11:02:02 -0500

    Finishing up BinarySearchTree.java,to then run and test it

---
+++ BinarySearchTree.java
@@ -0,0 +1,77 @@
+import java.util.Comparator;
+
+public class BinarySearchTree<Task> {
+    private Node root;
+    private Comparator<Task> com;
+
+    class Node{
+        private Task value;
+        private Node left;
+        private Node right;
+
+        Node(Task value){
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+
+    public BinarySearchTree(Comparator<Task> com){
+        this.com = com;
+    }
+
+    private Node insert(Node root,Task value){
+        int cmp;
+        if(root == null){
+            root = new Node(value);
+            return root;
+        }
+        cmp = com.compare(value, root.value);
+        if(cmp < 0){
+            root.left = insert(root.left, value);
+        } else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
+    private Node delete(Node root,Task value){
+        int cmp;
+        if(root == null){
+            return null;
+        }
+        cmp = com.compare(value, root.value);
+        if(cmp < 0){
+            root.left = delete(root.left, value);
+        } else if(cmp > 0){
+            root.right = delete(root.right, value);
+        } else{
+            if(root.left == null){
+                return root.right;
+            } if(root.right == null){
+                return root.left;
+            }
+        }
+        return root;
+    }
+
+    private Node search(Node root,Task value){
+        int cmp;
+        if(root == null){
+            return null;
+        }
+        cmp = com.compare(value, root.value);
+        if(cmp == 0){
+            return root;
+        } else if(cmp < 0) {
+            return search(root.left, value);
+        } else {
+            return search(root.right, value);
+        }
+    }
+
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+}
